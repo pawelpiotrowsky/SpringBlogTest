698d62b47bccf4ab07db63b13e3b5e2964f96141
pawelpiotrowsky <pawelpio96@gmail.com>
2021-05-20 14:46:18 +0200

    Redirect login test

--- src/main/java/com/example/SpringBlogTest/controller/LoginController.java
+++ src/main/java/com/example/SpringBlogTest/controller/LoginController.java
@@ -1,9 +1,5 @@
 package com.example.SpringBlogTest.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.web.DefaultRedirectStrategy;
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
-import org.springframework.security.web.savedrequest.RequestCache;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
