f523928bca5b13e317bdee8b7144111329153b70
pawelpiotrowsky <pawelpio96@gmail.com>
2021-05-20 16:13:48 +0200

    Redirect login test

--- src/main/java/com/example/SpringBlogTest/controller/LoginController.java
+++ src/main/java/com/example/SpringBlogTest/controller/LoginController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
-    DefaultRedirectStrategy defaultRedirectStrategy;
     @GetMapping("/login")
     public String getRegisterView() {
         return "login";
