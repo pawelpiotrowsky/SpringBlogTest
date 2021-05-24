cd087b8fefdff272fb8b4bd2a5227443f505c764
pawelpiotrowsky <pawelpio96@gmail.com>
2021-05-24 16:59:43 +0200

    .

--- src/main/java/com/example/SpringBlogTest/controller/HomePageController.java
+++ src/main/java/com/example/SpringBlogTest/controller/HomePageController.java
@@ -1,8 +1,11 @@
 package com.example.SpringBlogTest.controller;
 
 import com.example.SpringBlogTest.service.UserService;
+import com.example.SpringBlogTest.user.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping
@@ -15,14 +18,30 @@ public class HomePageController {
         this.userService = userService;
     }
 
-    @GetMapping ("/")
-    public String getMainView() { return "main";}
+        @GetMapping("/")
+        public String getMainView() { return "main";}
 
 
-    @GetMapping ("/error")
-    public String getErrorView() {
-        return "error";
-    }
+        @GetMapping ("/error")
+        public String getErrorView() {
+        return "error";}
+
+        @GetMapping("/login")
+        public String getRegisterView() {
+            return "login";}
+
+        @GetMapping("/passwordrecovery")
+        public String getPasswordRecovery() {return null;}
+
+        @GetMapping("/register")
+         public String getLoginView(Model model) {
+        model.addAttribute("user", new User());
+        return "register"; }
+
+        @PostMapping("/register")
+         public String createNewUser(User user) {
+        userService.addUser(user);
+        return "main"; }
 
 
 
